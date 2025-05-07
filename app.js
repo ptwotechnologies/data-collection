6cc22521663223979a6e475cf3c347ae8e4ce230
beingzuhairkhan <zuhairkhan5134@gmail.com>
2025-05-07 21:48:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:5179/',
+    origin: 'https://data-collection-40bw.onrender.com',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
