434261a02fc55310440894cd43a3baff66757d5a
Your Name <you@example.com>
2025-06-10 19:41:04 +0530

    removed the admin routes

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { registerAdmin, loginAdmin } from '../controllers/adminController.js'
+import { registerAdmin, loginAdmin } from '../controllers/adminController.js';
 
 const router = express.Router();
 
 // Admin Registration Route
-router.post('/register', registerAdmin);
+//for inital admin creation router.post('/register', registerAdmin);
 
 // Admin Login Route
 router.post('/login', loginAdmin);
